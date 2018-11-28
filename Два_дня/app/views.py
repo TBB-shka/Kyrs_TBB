c65d01696b38ed341f341d9b56170eb6af9743af
7161tav <ya.tomshin@gmail.com>
2018-11-28 17:28:20 +0700

    delete get name def

--- Два_дня/app/views.py
+++ Два_дня/app/views.py
@@ -114,28 +114,3 @@ def search(request):
     return HttpResponse(tre)
 
 
-
-
-
-
-
-
-from .forms import Data
-
-def get_name(request):
-    # if this is a POST request we need to process the form data
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = Data(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return HttpResponseRedirect('/thanks/')
-
-    # if a GET (or any other method) we'll create a blank form
-    else:
-        form = Data()
-
-    return render(request, 'Data.html', {'form': form})
